6cb4f8ca19054125a9b7e6bfdb5516d537911fad
Josue Maldonado <Memejosue2012@hotmail.com>
2022-07-05 20:21:17 -0600

    ultimos cambios

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ def nothing(x):
 
 
 img = cv.imread (cv.samples.findFile("cr7.jpg"))
-# Create a black image, a window
+# crea una imagen auxiliar de ceros
 img2 = np.zeros((img.shape[0],img.shape[1],img.shape[2]), np.uint8)
 
-cv.namedWindow('image')
-# create trackbars for color change
+cv.namedWindow('Imagen RGB')
+
+
+# crea las 4 trackbar
 cv.createTrackbar('R','image',0,100,nothing)
 cv.createTrackbar('G','image',0,100,nothing)
 cv.createTrackbar('B','image',0,100,nothing)
-# create switch for ON/OFF functionality
+# creamos el switch on/off
 switch = '0 : OFF \n1 : ON'
 cv.createTrackbar(switch, 'image',0,1,nothing)
 while(1):
@@ -22,7 +24,7 @@ while(1):
     k = cv.waitKey(1) & 0xFF
     if k == 27:
         break
-    # get current positions of four trackbars
+    # obtinene la posicion
     r = cv.getTrackbarPos('R','image')
     g = cv.getTrackbarPos('G','image')
     b = cv.getTrackbarPos('B','image')
